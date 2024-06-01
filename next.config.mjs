d431abc51fc2697b7aac20f838f8e1e4fb8eca77
Trevor Farias <levelonethousandr@gmail.com>
2024-05-31 17:27:12 -0700

    good to push

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPayload(nextConfig);
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ReduxProviders} from "./GlobalRedux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><ReduxProviders>{children}</ReduxProviders></body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
