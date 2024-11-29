92a2c05a7728713d74e70b25b065d1e9df7dfb1b
Trevor Farias <levelonethousandr@gmail.com>
2024-11-29 01:22:44 -0500

    Build issues fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,25 @@
 import { MongoClient } from 'mongodb';
 
+
 const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      _mongoClientPromise?: Promise<MongoClient>;
-    }
-  }
+
+if(!uri || !process.env.MONGODB_URI){
+  throw new Error('Please add URI to .env')
 }
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local');
-}
+
 
 if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
+  if (!globalThis._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalThis._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalThis._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
