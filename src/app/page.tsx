2fb6201cc2dc21f6fa920dc38bbc6070a8136f1e
Trevor Farias <levelonethousandr@gmail.com>
2024-11-29 18:51:14 -0500

    Temporarily closed CLASSIC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import GameModeContainer from "./components/GameModeContainer";
 import { GameMode } from "../../types";
 
 const Page = () => {
-    const gameModes: GameMode[] = ["classic", "leaderboard"]
+    const gameModes: GameMode[] = ["leaderboard"]
     const dispatch = useDispatch();
     const colors = [
         "bg-purple-600",
@@ -17,9 +17,8 @@ const Page = () => {
         <div className="bg-white p-6 rounded-lg w-11/12 md:w-3/4 lg:w-1/2 xl:w-1/3">
             <div className="flex flex-col">
                 <div className="flex justify-center items-center pb-12 h-1/2">
-                    <h1 className="text-3xl">Select Gamemode</h1>
+                    <h1 className="text-3xl">Ready to play?</h1>
                 </div>
-
                 <div className="flex flex-row h-1/2 gap-4 text-center">
             
                         {
