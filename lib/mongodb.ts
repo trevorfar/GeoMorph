9dc0c848b32bd2f37c2c52547ea3ffa4cebe0be7
trevorfar <21tof1@queensu.ca>
2024-12-11 21:54:57 -0500

    Dsiabled mongo temporarily

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,24 +1,24 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-const options = {};
+// const uri = process.env.MONGODB_URI;
+// const options = {};
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+// let client: MongoClient;
+// let clientPromise: Promise<MongoClient>;
 
-if (!uri || !process.env.MONGODB_URI) {
-  throw new Error("Please add URI to .env");
-}
+// if (!uri || !process.env.MONGODB_URI) {
+//   throw new Error("Please add URI to .env");
+// }
 
-if (process.env.NODE_ENV === "development") {
-  if (!globalThis._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalThis._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalThis._mongoClientPromise as Promise<MongoClient>;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   if (!globalThis._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     globalThis._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = globalThis._mongoClientPromise as Promise<MongoClient>;
+// } else {
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
 
-export default clientPromise;
+// export default clientPromise;
